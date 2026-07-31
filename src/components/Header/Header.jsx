da072f59db1f5824675feb1bb2638965abb42470
Joao Franca <jvictorfranca@yahoo.com.br>
2026-07-31 19:08:11 -0300

    adds header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import "./Header.css"
+import { House, Phone, Mail } from 'lucide-react';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Cambrige Rentals</h1>
+        <header className="header">
+            <div className="item">
+                <House className="icon"/>
+                <span>Cambrige Rentals</span>
+            </div>
+            <div className="item">
+                <Phone className="icon"/>
+                <span>12 3456-7890</span>
+            </div>
+            <div className="item">
+                <Mail className="icon"/>
+                <span>jvictorfranca@yahoo.com.br</span>
+            </div>
         </header>
     )
  }
